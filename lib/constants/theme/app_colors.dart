e1aa75cb5368b21b6154f9ac361fabe0c084645c
Taylon Luis <106061516+TaylonLu@users.noreply.github.com>
2025-12-10 21:18:47 -0300

    Adicionado Tela de Informações sobre a conta (pessoa física e jurídica)

--- lib/constants/theme/app_colors.dart
+++ lib/constants/theme/app_colors.dart
@@ -6,12 +6,14 @@ class AppColors {
 
   // --- Paleta Principal ---
   
-  static const Color background = Color(0xFFD4E0D4); 
+  static const Color background = Color(0xFFD4E0D4);
+  static const Color header = Color(0xFF586C61);
   static const Color title = Color(0xFF1C2F0F); // Verde escuro para títulos
+  static const Color textColor = Colors.black;
   static const Color textBody = Color(0xFF2F332C); // Verde médio para textos do corpo
   static const Color primary = Color(0xFF284017);
   static const Color amarelo = Color(0xFFF69B0F);
   static const Color primaria = Color(0xFF2F4838); // Verde escuro
   static const Color fundo = Color(0xFFD9E5D6); // Verde claro
-  
+  static const Color infoDoc = Color(0xFFBFCDBD); // Verde claro
 }
